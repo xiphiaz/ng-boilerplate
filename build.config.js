b6d54a3aa9336fcf55ddc4ec877272b0997942a6
Zak Henry <zak.henry@gmail.com>
2013-10-08 00:42:13 +1100

    Began install of grunt-wtml, rewrote the index.html file in wtml.

--- build.config.js
+++ build.config.js
@@ -30,7 +30,9 @@ module.exports = {
     ctpl: [ 'src/common/**/*.tpl.html' ],
 
     html: [ 'src/index.html' ],
-    less: 'src/less/main.less'
+    less: 'src/less/main.less',
+
+    wtml: ['src/**/*.wtml']
   },
 
   /**
